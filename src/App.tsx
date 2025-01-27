20672466f6bedbda173f29b86d18da4b0fe51a68
Low Front <lowfronterior@gmail.com>
2025-01-28 03:19:06 +0900

    chore: add examples

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ import "./Knob.css"
 function App() {
   return (
     <div>
-      <Knob />
+      <Knob
+        minAngle={0}
+        maxAngle={300}
+        minValue={0}
+        maxValue={1}
+        stepAngle={60}
+        startAngle={210}
+      />
+      <Knob
+        minAngle={0}
+        maxAngle={270}
+        minValue={0}
+        maxValue={1}
+        defaultValue={0.3}
+        startAngle={225}
+      />
     </div>
   )
 }

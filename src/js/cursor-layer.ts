2b760e298c6ca0d4414238ae48cb0ba7766396bd
Low Front <lowfronterior@gmail.com>
2025-01-28 19:18:30 +0900

    feat: lazy createElement cause SSR

--- src/js/cursor-layer.ts
+++ src/js/cursor-layer.ts
@@ -1,52 +1,59 @@
-const cursorLayer = Object.assign(document.createElement("div"), {
-  style: `position: fixed; width: 100vw; height: 100vh; top: 0; left: 0; z-index: 99999999; display: none;`,
-})
-document.body.appendChild(cursorLayer)
+let cursorLayer: HTMLDivElement
 
 type CursorType =
-  | "alias"
-  | "all-scroll"
-  | "auto"
-  | "cell"
-  | "col-resize"
-  | "context-menu"
-  | "copy"
-  | "crosshair"
-  | "default"
-  | "e-resize"
-  | "ew-resize"
-  | "grab"
-  | "grabbing"
-  | "help"
-  | "move"
-  | "n-resize"
-  | "ne-resize"
-  | "nesw-resize"
-  | "ns-resize"
-  | "nw-resize"
-  | "nwse-resize"
-  | "no-drop"
-  | "none"
-  | "not-allowed"
-  | "pointer"
-  | "progress"
-  | "row-resize"
-  | "s-resize"
-  | "se-resize"
-  | "sw-resize"
-  | "text"
-  | "url"
-  | "w-resize"
-  | "wait"
-  | "zoom-in"
-  | "zoom-out"
+  | 'alias'
+  | 'all-scroll'
+  | 'auto'
+  | 'cell'
+  | 'col-resize'
+  | 'context-menu'
+  | 'copy'
+  | 'crosshair'
+  | 'default'
+  | 'e-resize'
+  | 'ew-resize'
+  | 'grab'
+  | 'grabbing'
+  | 'help'
+  | 'move'
+  | 'n-resize'
+  | 'ne-resize'
+  | 'nesw-resize'
+  | 'ns-resize'
+  | 'nw-resize'
+  | 'nwse-resize'
+  | 'no-drop'
+  | 'none'
+  | 'not-allowed'
+  | 'pointer'
+  | 'progress'
+  | 'row-resize'
+  | 's-resize'
+  | 'se-resize'
+  | 'sw-resize'
+  | 'text'
+  | 'url'
+  | 'w-resize'
+  | 'wait'
+  | 'zoom-in'
+  | 'zoom-out'
 
 export const cursor = {
   show(cursorType: CursorType) {
-    cursorLayer.style.display = "block"
-    cursorLayer.style.setProperty("cursor", cursorType, "important")
+    if (!cursorLayer) {
+      cursorLayer = Object.assign(document.createElement('div'), {
+        style: `position: fixed; width: 100vw; height: 100vh; top: 0; left: 0; z-index: 99999999; display: none;`,
+      })
+      document.body.appendChild(cursorLayer)
+    }
+
+    cursorLayer.style.display = 'block'
+    cursorLayer.style.setProperty('cursor', cursorType, 'important')
   },
   hide() {
-    cursorLayer.style.display = "none"
+    if (!cursorLayer) {
+      return
+    }
+    cursorLayer.style.display = 'none'
   },
 }

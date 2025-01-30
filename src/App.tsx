034d0252d9ddafb1efd7280489f98de17a20319e
Low Front <lowfronterior@gmail.com>
2025-01-31 01:08:37 +0900

    chore: update example props

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 
 function App() {
   const [value, setValue] = useState(0)
+  const [value2, setValue2] = useState(0)
 
   return (
     <div>
@@ -19,7 +20,7 @@ function App() {
           minValue={0}
           maxValue={5}
           startAngle={210}
-          stepAngle={60}
+          stepValue={1}
           onStatusChange={console.log}
         >
           <button type="button" className="knob" />
@@ -59,15 +60,38 @@ function App() {
           onChange={(e) => setValue(Number(e.target.value))}
         />
         <Knob
-          defaultValue={0.3}
+          defaultValue={value}
           value={value}
           onValueChange={setValue}
           minAngle={0}
           maxAngle={270}
           minValue={0}
           maxValue={1}
-          stepAngle={27}
+          stepValue={0.1}
           startAngle={225}
+        >
+          <button type="button" className="knob" />
+        </Knob>
+      </div>
+      <div>
+        <input
+          type="number"
+          min={0}
+          max={5}
+          step={1}
+          value={value2}
+          onChange={(e) => setValue2(Number(e.target.value))}
+        />
+        <Knob
+          defaultValue={0}
+          value={value2}
+          onValueChange={setValue2}
+          minAngle={0}
+          maxAngle={300}
+          minValue={0}
+          maxValue={5}
+          stepValue={1}
+          startAngle={210}
         >
           <button type="button" className="knob" />
         </Knob>

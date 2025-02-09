2eea1a3fafe4f12bef7b75eb2d8a55d9602de365
fronterior <lowfronterior@gmail.com>
2025-02-09 23:57:18 +0900

    feat: update js example

--- src/examples/js/index.ts
+++ src/examples/js/index.ts
@@ -1,1 +1,20 @@
-export { }
+import { Knob } from '../../js'
+
+console.log(Knob)
+
+const div = Object.assign(document.createElement('div'), {
+  style: 'width: 100px; height: 100px; background: salmon;',
+})
+
+document.body.append(div)
+
+const knob = new Knob(div, {
+  defaultValue: 1,
+  minValue: 1,
+  maxValue: 7,
+  minDegrees: 0,
+  maxDegrees: 270,
+  onValueChange: console.log,
+})
+
+console.log(knob)

732162e21eabebd8e9d21573ee9776aa35121a7d
fronterior <lowfronterior@gmail.com>
2025-02-10 23:40:21 +0900

    feat: update example

--- src/examples/js/index.ts
+++ src/examples/js/index.ts
@@ -4,6 +4,7 @@ console.log(Knob)
 
 const div = Object.assign(document.createElement('div'), {
   style: 'width: 100px; height: 100px; background: salmon;',
+  innerText: 'A',
 })
 
 document.body.append(div)
@@ -11,9 +12,11 @@ document.body.append(div)
 const knob = new Knob(div, {
   defaultValue: 1,
   minValue: 1,
-  maxValue: 7,
+  maxValue: 14,
   minDegrees: 0,
   maxDegrees: 270,
+  stepValue: 1,
+  startDegrees: 180,
   onValueChange: console.log,
 })
 
